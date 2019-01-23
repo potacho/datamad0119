b2c1138ae26a3099ae4c919f12adcdba9f276e02
potacho <potacho@gmail.com>
2019-01-23 14:38:08 +0100

    Challenge 2 done!

--- module-1/lab-mysql-select/your-code/lab-mysql-select.sql
+++ module-1/lab-mysql-select/your-code/lab-mysql-select.sql
@@ -36,6 +36,31 @@ INNER JOIN publishers ON titles.pub_id = publishers.pub_id;
 
 #Challenge 2 - Who Have Published How Many At Where?
 
+#Sin usar INNER JOIN
+SELECT 
+authors.au_id AS 'Author ID', 
+authors.au_lname AS 'Last Name', 
+authors.au_fname AS 'First Name', 
+ANY_VALUE(publishers.pub_name) AS 'Book Publisher', 
+COUNT(titleauthor.au_id) AS 'Number of Titles'
+FROM authors, titleauthor, titles, publishers 
+WHERE titles.title_id=titleauthor.title_id 
+AND titleauthor.au_id=authors.au_id AND titles.pub_id=publishers.pub_id
+GROUP BY authors.au_id;
+
+#Usando INNER JOIN
+SELECT
+authors.au_id AS 'Author ID', 
+authors.au_lname AS 'Last Name', 
+authors.au_fname AS 'First Name', 
+publishers.pub_name AS 'Book Publisher',
+COUNT(titleauthor.au_id) AS 'Number of Titles'
+FROM authors
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers ON titles.pub_id = publishers.pub_id
+GROUP BY authors.au_id, authors.au_fname, authors.au_lname, publishers.pub_name;
+
 
 
 #Challenge 3 - Best Selling Authors
