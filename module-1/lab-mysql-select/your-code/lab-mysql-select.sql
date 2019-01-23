0e376aba64a7485ad2b34cd16cc12f9026969060
potacho <potacho@gmail.com>
2019-01-23 13:42:17 +0100

    Descargo fichero 'publications.sql', importo datos en Workbench y creo fichero 'lab-mysql-select.org' para realizar el lab

---
+++ module-1/lab-mysql-select/your-code/lab-mysql-select.sql
@@ -0,0 +1,23 @@
+#[lab-mysql-select]octavio
+
+#Descargo fichero 'publications.sql'
+#Importo datos utilizando el wizard de Workbench: Administration → Data Import/Restore → Import from Self-Contained File → Select File → Default Target Schema → Start Import
+
+#Challenge 1 - Who Have Published What At Where?
+
+
+
+#Challenge 2 - Who Have Published How Many At Where?
+
+
+
+#Challenge 3 - Best Selling Authors
+
+
+
+#Challenge 4 - Best Selling Authors Ranking
+
+
+
+#Bonus Challenge - Most Profiting Authors
+
