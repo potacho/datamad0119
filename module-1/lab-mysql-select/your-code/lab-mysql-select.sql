3d576f94c73046b9c400064b3ab6b798f8f412ea
potacho <potacho@gmail.com>
2019-01-24 01:35:16 +0100

    Challenge 4 done!

--- module-1/lab-mysql-select/your-code/lab-mysql-select.sql
+++ module-1/lab-mysql-select/your-code/lab-mysql-select.sql
@@ -100,6 +100,19 @@ LIMIT 3;
 
 
 #Challenge 4 - Best Selling Authors Ranking
+SELECT 
+authors.au_id AS 'Author ID', 
+authors.au_lname AS 'Last Name', 
+authors.au_fname AS 'First Name',
+#titleauthor.title_id, 
+#titles.title, sales.qty,
+COALESCE(SUM(sales.qty),0) AS 'Total'
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id, authors.au_lname, authors.au_fname
+ORDER BY SUM(sales.qty) DESC, authors.au_id ASC;
 
 
 
