ded375f8c3adf9c628be734201b4e2641e07da27
potacho <potacho@gmail.com>
2019-02-05 18:31:36 +0100

    Empiezo de nuevo

--- module-1/lab-api-scavenger-game/your-code/scavenger.py
+++ module-1/lab-api-scavenger-game/your-code/scavenger.py
@@ -1,1 +1,18 @@
 from dotenv import load_dotenv
+import os
+import json
+import requests
+import pandas as pd
+
+# KEY
+load_dotenv(dotenv_path='../.env')
+SECRET_KEY = os.getenv("KEY")
+
+source = "https://github.com/ironhack-datalabs/mad-oct-2018/forks"
+labs = requests.get(source)
+datos = labs.json()
+
+#forks = datos["forks_count"]
+
+#forks = list(set([key for keys in datos for key in datos]))
+print(datos)
